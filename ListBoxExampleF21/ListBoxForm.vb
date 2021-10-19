8544476ddfcd7c242fdbcd0a495606be45b8ecb3
rosstimo <rosstimo@isu.edu>
2021-10-19 08:51:51 -0600

    add combo box

--- ListBoxExampleF21/ListBoxForm.vb
+++ ListBoxExampleF21/ListBoxForm.vb
@@ -5,9 +5,17 @@
 
     Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
         ListBox1.Items.Add(TextBox1.Text)
+        ComboBox1.Items.Add(TextBox1.Text)
+        If ComboBox1.Items.Count = 1 Then
+            ComboBox1.SelectedIndex = 0
+        End If
     End Sub
 
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
-        Me.Text = $"Index {ListBox1.SelectedIndex} contains: {ListBox1.SelectedItem}"
+        Me.Text = $"The list box selected index is {ListBox1.SelectedIndex} and it contains: {ListBox1.SelectedItem}"
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Me.Text = $"The combo box selected index is {ComboBox1.SelectedIndex} and it contains: {ComboBox1.SelectedItem}"
     End Sub
 End Class
