08d68f753f0dfabf323a98152d271577f0aafe37
rosstimo <rosstimo@isu.edu>
2021-10-19 08:37:30 -0600

    add list box control examples

--- ListBoxExampleF21/ListBoxForm.Designer.vb
+++ ListBoxExampleF21/ListBoxForm.Designer.vb
@@ -22,10 +22,74 @@ Partial Class ListBoxForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.UpdateButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(312, 66)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(447, 277)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(53, 66)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(253, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'UpdateButton
+        '
+        Me.UpdateButton.Location = New System.Drawing.Point(469, 349)
+        Me.UpdateButton.Name = "UpdateButton"
+        Me.UpdateButton.Size = New System.Drawing.Size(142, 71)
+        Me.UpdateButton.TabIndex = 2
+        Me.UpdateButton.Text = "&Update"
+        Me.UpdateButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(617, 349)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(142, 71)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(53, 94)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'ListBoxForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.UpdateButton)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "ListBoxForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents UpdateButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- ListBoxExampleF21/ListBoxForm.vb
+++ ListBoxExampleF21/ListBoxForm.vb
@@ -1,3 +1,13 @@
 ﻿Public Class ListBoxForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        ListBox1.Items.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Me.Text = $"Index {ListBox1.SelectedIndex} contains: {ListBox1.SelectedItem}"
+    End Sub
 End Class
